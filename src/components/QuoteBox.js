dd106e7118ae0bd54d2e158a233f05e5b0b61d43
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-14 15:30:59 -0300

    final

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -5,28 +5,30 @@ export default function QuoteBox({ quote, handleNewQuote }) {
     <main className="flex flex-col items-center justify-center min-h-screen">
       <div className="p-8 max-w-md mx-auto bg-white shadow-md rounded-md">
         <section>
-          <h3 className="my-8 text-2xl font-bold">
-            <span className="text-gray-500">&ldquo;</span>
-            {quote?.text}
-            <span className="text-gray-500">&rdquo;</span>
+          <h3 className="my-8 text-start">
+            <p className="text-2xl font-bold">
+              <span className="text-gray-500">&ldquo;</span>
+              {quote?.text}
+              <span className="text-gray-500">&rdquo;</span>
+            </p>
           </h3>
-          <h2 className="font-medium">{quote?.author}</h2>
+          <h2 className="mb-8 text-end">-{quote?.author}</h2>
         </section>
-        <section className="flex justify-around items-center mt-8">
+        <div className="flex flex-wrap justify-around items-center">
           <button
             onClick={handleNewQuote}
-            className="px-4 py-2 mt-8 text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none"
+            className="w-full md:w-auto px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 focus:outline-none mb-4 md:mb-0"
           >
-            New Quote
+            New
           </button>
-          <button
+          <a
             href="https://twitter.com/intent/tweet"
             target="blank"
-            className="px-4 py-2 mt-8 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none"
+            className="w-full md:w-auto px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none mb-4 md:mb-0"
           >
             Tweet
-          </button>
-        </section>
+          </a>
+        </div>
       </div>
     </main>
   );
